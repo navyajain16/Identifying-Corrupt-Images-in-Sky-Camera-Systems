fc16032d3ff6f4b18f846f41a710f5bdbf1fb444
navyajain16 <72208843+navyajain16@users.noreply.github.com>
2022-03-12 12:56:27 +0530

    Update and rename SVMLOOP.py to Classification.py

--- Classification.py
+++ Classification.py
@@ -1,9 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Aug 14 00:54:26 2021
-
-@author: navya
-"""
 import os 
 import numpy as np
 import cv2
@@ -17,33 +11,34 @@ from sklearn import metrics
 from skimage.color import rgb2hsv, rgb2gray, rgb2yuv
 from skimage.measure.entropy import shannon_entropy
 import shutil
-from sklearn.tree import DecisionTreeClassifier
 import pandas as pd
 import glob
-import time
 
-start = time.time()
-
+#Defining function for Laplacian
 def variance_of_laplacian(image):
         return cv2.Laplacian(image, cv2.CV_64F).var()
 
 data=[]
 
+#Loading the trained model
 pick = open('modeclouds.sav', 'rb')
 model = pickle.load(pick)
 pick.close() 
 
-
-Categories =['badimg','goodimg']
-
+#Path for source dataset 
 src ="test"
+#Path for directory created for storing corrupt images
 dub ="bad"
  
-catimg=[]
+corimg=[]
+
+#Creating labels
+Categories =['badimg','goodimg']
 
 for category in Categories:
         label = Categories.index(category)
-        
+
+#Images passed through various steps of the framework
 for image in os.listdir(src): 
         cloud_img = cv2.imread(image)
         #gray = cv2.cvtColor(cloud_img, cv2.COLOR_BGR2GRAY)
@@ -57,26 +52,29 @@ for image in os.listdir(src):
         finalimg = np.array([jimage,img])
         fimg = finalimg.flatten() 
         data.append([fimg,label])  
-        catimg.append(image)
-        #print(data)
+        corimg.append(image)
+        
 print(len(data))
 features= [] 
 labels = [] 
+
+#Modifying the data to be predicted by the model
 for feature,label in data:
         features.append(feature) 
         labels.append(label)
 images = np.asarray((list(sub[1] for sub in features))) 
 images=images.reshape(len(images),3) 
+
+#Predicting the data
 probability=model.predict_proba(images)
 ypred = model.predict(images)
-#print(probability)
 dec = np.asarray((list(sub[0] for sub in probability)))
+
+#Moving corrupt images to seperate directory
 for value,img in zip(ypred,catimg):
     if value == 0:
         print(img + " " + "bad")
         shutil.move(os.path.join(src, img), dub)
     else:
         print("good")
-        
-stop = time.time()
-print(stop-start)
+
